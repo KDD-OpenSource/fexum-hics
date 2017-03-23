d99b286b08d594b00882a8d1690ec8e0969389d1
Niklas Riekenbrauck <nikriek@users.noreply.github.com>
2017-03-23 17:04:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
 	name='hics', 
-	version='0.6',                  
+	version='0.7',                  
 	author='Marcus Pappik',
 	install_requires=[
 		'pandas',
