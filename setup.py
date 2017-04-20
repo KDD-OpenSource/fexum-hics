4b3d4110d9d50fe2f349b1df49fbb0e3a2d6c5f3
marcuspappik <marcus.pappik@student.hpi.uni-potsdam.de>
2017-04-20 16:20:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
 	name='hics', 
-	version='0.11',                  
+	version='0.12',                  
 	author='Marcus Pappik',
 	install_requires=[
 		'pandas',
